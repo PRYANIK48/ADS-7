e72043bcb5323f47479497bfdfa3ed2bbef72412
PRYANIK48 <112880306+PRYANIK48@users.noreply.github.com>
2026-05-29 19:09:53 +0300

    Implement Train class with car management methods

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,60 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train()
+{
+    first = nullptr;
+    countOp = 0;
+}
+
+void Train::addCar(bool light)
+{
+    Car* car = new Car();
+    car->light = light;
+    if (first) {
+        car->prev = first->prev;
+        first->prev->next = car;
+    } else {
+        first = car;
+    }
+    first->prev = car;
+    car->next = first;
+}
+
+int Train::getLength()
+{
+    if (!first) {
+        return 0;
+    }
+    bool flag = true;
+    int length = 0;
+    first->light = true;
+    while (flag) {
+        int distance = 0;
+
+        do {
+            distance++;
+            countOp++;
+            first = first->next;
+        } while (first->light == false);
+
+        first->light = false;
+        length = distance;
+
+        for (; distance > 0; distance--) {
+            std::cout << distance << std::endl;
+            countOp++;
+            first = first->prev;
+            std::cout << "end" << std::endl;
+        }
+        if (first->light == false) {
+            flag = false;
+        }
+    }
+    return length;
+}
+
+int Train::getOpCount()
+{
+    return countOp;
+}
