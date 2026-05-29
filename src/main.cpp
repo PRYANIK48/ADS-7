109107c3c8efb625c52ecc5d645ad04a77f991f5
PRYANIK48 <112880306+PRYANIK48@users.noreply.github.com>
2026-05-29 19:39:06 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,29 @@
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+    Train train;
+    int count = 100; // кол-во вагонов
+    int type = 3;
+    srand((time(0)));
+    switch (type)
+    {
+    case 1:
+        while (count--)
+            train.addCar(false);
+        break;
+    case 2:
+        while (count--)
+            train.addCar(true);
+        break;
+    case 3:
+        while (count--)
+            train.addCar(rand()%2);
+        break;
+    default:
+        break;
+    }
 
-  while (count--)
-    train.addCar(false);
-
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
-  return 0;
+    std::cout << train.getLength() << std::endl;
+    std::cout << train.getOpCount() << std::endl;
+    return 0;
 }
